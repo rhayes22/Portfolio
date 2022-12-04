eb2f3ba25842c358e8317b40f87dce6fb89c9e65
Ryan Hayes <rrh5210@psu.edu>
2022-12-04 13:47:58 -0500

    Initial setup finished

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      Ryan Hayes is a Pennsylvania State University Graduate in the class of
+      2022.
+    </p>
+  </div>
+);
+
+export default AboutPage;
---
+++ src/components/projects/ProjectPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ProjectPage = () => (
+  <div>
+    <h2>Projects</h2>
+    <p>Below are all projects completed by Ryan Hayes.</p>
+  </div>
+);
+
+export default ProjectPage;
